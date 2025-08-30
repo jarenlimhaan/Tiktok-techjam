95fe26b7b5dd5b5378c920c40c0c554668a2e9bc
Jaren <kaylajaren@gmail.com>
2025-08-30 14:03:32 +0800

    chore: update env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const nextConfig: NextConfig = {
   /* config options here */
+
 };
 
 export default nextConfig;
